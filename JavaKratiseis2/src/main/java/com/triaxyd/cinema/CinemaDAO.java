b8b5c8a8d1e04bc5e735a4f9628272e746343d70
Triantafyllos Xydis <97985533+triaxyd@users.noreply.github.com>
2023-05-20 23:59:33 +0300

    Delete JavaKratiseis2 directory

--- JavaKratiseis2/src/main/java/com/triaxyd/cinema/CinemaDAO.java
+++
@@ -1,9 +0,0 @@
-package com.triaxyd.cinema;
-
-public class CinemaDAO {
-
-    public Movies checkMovie(String title,String content,String length,String type,String summary,String director,String content_admin_ID){
-        Movies movie = null;
-        return movie;
-    }
-}
